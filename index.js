02378ecd22dcb8572a5b3af57039dd3276600172
IhorLev1 <lambermen1@gmail.com>
2020-09-24 20:57:31 +0300

    add

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const refs = {
+  days: document.querySelector(".value[data-value=days]"),
+  hours: document.querySelector(".value[data-value=hours]"),
+  mins: document.querySelector(".value[data-value=mins]"),
+  secs: document.querySelector(".value[data-value=secs]"),
+};
+
+const timer = {
+  intervalId: null,
+  targetDate: new Date("11/25/2020"),
+  start() {
+    updateClockface(0);
+
+    this.intervalId = setInterval(() => {
+      const startTime = Date.now();
+      const deltaTime = this.targetDate - startTime;
+
+      updateClockface(deltaTime);
+    }, 1000);
+    if (deltaTime <= 0) {
+      clearInterval(this.intervalId);
+    }
+  },
+};
+function updateClockface(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+timer.start();
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
